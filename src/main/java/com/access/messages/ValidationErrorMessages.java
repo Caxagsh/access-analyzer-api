2c74e480c789adfc6f8b46052b7599d6064202c9
allancrowley <allancrowleydev@gmail.com>
2024-05-25 10:27:39 +0300

    updates DTO validation error messages

---
+++ src/main/java/com/access/messages/ValidationErrorMessages.java
@@ -0,0 +1,9 @@
+package com.access.messages;
+
+public interface ValidationErrorMessages {
+    String INVALID_SUBNET_FORMAT_MSG = "Subnet format is invalid";
+    String MISSING_SUBNET_MSG = "Subnet number is missing";
+    String MISSING_SERVICE_NAME_MSG = "Service name is missing";
+    String MISSING_SERVICE_EMAIL_MSG = "Service email is missing";
+    String INVALID_EMAIL_FORMAT_MSG = "Email format is invalid";
+}
