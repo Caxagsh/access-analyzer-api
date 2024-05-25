60e7fa784053e373886115269d37417fb5ef29e3
allancrowley <allancrowleydev@gmail.com>
2024-05-25 09:07:02 +0300

    updates DTO field names

--- src/main/java/com/access/dto/AttackAttemptDto.java
+++ src/main/java/com/access/dto/AttackAttemptDto.java
@@ -1,5 +1,5 @@
 package com.access.dto;
 
 //DTO for transfering data about attack attempt (authentication failures sequence which recognized as attack)
-public record AttackAttemptDto(String ip, Long timestamp, String[] services) {
+public record AttackAttemptDto(String subnet, Long timestamp, String[] services) {
 }
--- src/main/java/com/access/dto/AuthFailureDto.java
+++ src/main/java/com/access/dto/AuthFailureDto.java
@@ -1,5 +1,5 @@
 package com.access.dto;
 
 //DTO for transfering data about authentication failures (transfers for analyzing for attack possibility)
-public record AuthFailureDto(String ip, Long timestamp, String webserviceName) {
+public record AuthFailureDto(String subnet, Long timestamp, String webserviceName) {
 }
