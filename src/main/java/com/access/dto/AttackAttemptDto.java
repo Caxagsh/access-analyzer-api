8aa543e13dd5b9e943a554afb7453730e297239a
allancrowley <allancrowleydev@gmail.com>
2024-05-31 10:52:42 +0200

    exchanges array of services in AttackAttemptDto to list

--- src/main/java/com/access/dto/AttackAttemptDto.java
+++ src/main/java/com/access/dto/AttackAttemptDto.java
@@ -1,12 +1,13 @@
 package com.access.dto;
 
 import jakarta.validation.constraints.*;
+import java.util.List;
 import static com.access.ConstraintConstants.*;
 import static com.access.messages.ValidationErrorMessages.*;
 
-//DTO for transfering data about attack attempt (authentication failures sequence which recognized as attack)
+//DTO for transferring data about attack attempt (authentication failures sequence which recognized as attack)
 public record AttackAttemptDto(
         @NotEmpty(message = MISSING_SUBNET_MSG)
         @Pattern(regexp = IP_SUBNET_REGEXP, message = INVALID_SUBNET_FORMAT_MSG) String subnet,
-        Long timestamp, String[] services) {
+        Long timestamp, List<String> services) {
 }
