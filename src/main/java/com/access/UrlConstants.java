e59505a911d11038132c8f6ea59fd6474fe3d8ed
allancrowley <allancrowleydev@gmail.com>
2024-06-17 10:12:37 +0300

    adds url constant for new module

--- src/main/java/com/access/UrlConstants.java
+++ src/main/java/com/access/UrlConstants.java
@@ -4,4 +4,5 @@ public interface UrlConstants {
     String IP_PATH = "ip";
     String EMAIL_PATH = "email";
     String SERVICE_PATH = "service";
+    String AUTH_FAILURE_ROUTING_PATH = "route";
 }
